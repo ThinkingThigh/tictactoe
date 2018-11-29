a25073cca4287de24ec805096a56d9afd562fbc3
thinkingthigh <thinkingthigh@gmail.com>
2018-11-29 18:12:18 +0800

    函数定义组件，事件处理函数的写法，轮流执子

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square" onClick={() => this.props.onClick()}>
-                {
-                    this.props.value
-                }
-            </button>
-        );
-    }
+
+//现在格子组件 Square 不再拥有自身的状态数据了。它从棋盘父组件 Board 接受数据，并且当自己被点击时通知触发父组件改变状态数据，我们称这类的组件为 受控组件。
+// class Square extends React.Component {
+//     render() {
+//         return (
+//             <button className="square" onClick={() => this.props.onClick()}>
+//                 {
+//                     this.props.value
+//                 }
+//             </button>
+//         );
+//     }
+// }
+
+//React 专门为像 Square 组件这种只有 render 方法的组件提供了一种更简便的定义组件的方法： 函数定义组件 。只需要简单写一个以 props 为参数的 function 返回 JSX 元素就搞定了。
+//事件处理函数的写法，这里我们把 onClick={() => props.onClick()} 直接修改为 onClick={props.onClick} , 注意不能写成 onClick={props.onClick()} 否则 props.onClick 方法会在 Square 组件渲染时被直接触发而不是等到 Board 组件渲染完成时通过点击触发，又因为此时 Board 组件正在渲染中（即 Board 组件的 render() 方法正在调用），又触发 handleClick(i) 方法调用 setState() 会再次调用 render() 方法导致死循环。
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -20,6 +32,8 @@ class Board extends React.Component {
         //状态提升：当你遇到需要同时获取多个子组件数据，或者两个组件之间需要相互通讯的情况时，把子组件的 state 数据提升至其共同的父组件当中保存。之后父组件可以通过 props 将状态数据传递到子组件当中。这样应用当中的状态数据就能够更方便地交流共享了。
         this.state = {
             squares: Array(9).fill(null),
+            //记录轮流落子状态
+            xIsNext: true,
         };
     }
     renderSquare(i) {
@@ -32,8 +46,19 @@ class Board extends React.Component {
         );
     }
 
+    handleClick(i) {
+        //我们使用了 .slice() 方法来将之前的数组数据浅拷贝到了一个新的数组中，而不是修改已有的数组。
+        const squares = this.state.squares.slice();
+        //轮流执子
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
